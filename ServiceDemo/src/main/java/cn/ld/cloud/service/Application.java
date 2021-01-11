4348700501d179ae1535a01bbe7a8b56a92ba4c1
superHeroLD <superhero_L@163.com>
2021-01-11 20:50:13 +0800

    1.[A] add service demo

---
+++ ServiceDemo/src/main/java/cn/ld/cloud/service/Application.java
@@ -0,0 +1,18 @@
+package cn.ld.cloud.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author lidong
+ * @date 2021/1/11
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
